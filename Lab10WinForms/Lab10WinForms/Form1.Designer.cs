74c58d234ee818c33bc003a8d4337f804cfb8f99
kirill-lukyanov <71383135+kirill-lukyanov@users.noreply.github.com>
2022-12-28 21:29:36 +0300

    lab10

---
+++ Lab10WinForms/Lab10WinForms/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Lab10WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title5 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title6 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.BorderColor = System.Drawing.Color.BlanchedAlmond;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Низкое";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Среднее";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Высокое";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(425, 233);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Количество выдач книг";
+            title2.Name = "Title2";
+            this.chart1.Titles.Add(title1);
+            this.chart1.Titles.Add(title2);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(594, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(594, 376);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(101, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(591, 360);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // chart2
+            // 
+            chartArea2.BorderColor = System.Drawing.Color.BlanchedAlmond;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(443, 12);
+            this.chart2.Name = "chart2";
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Низкое";
+            series5.BorderWidth = 3;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Среднее";
+            series6.BorderWidth = 3;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "Высокое";
+            this.chart2.Series.Add(series4);
+            this.chart2.Series.Add(series5);
+            this.chart2.Series.Add(series6);
+            this.chart2.Size = new System.Drawing.Size(425, 233);
+            this.chart2.TabIndex = 7;
+            this.chart2.Text = "chart2";
+            title3.Name = "Title1";
+            title3.Text = "Место книги";
+            title4.Name = "Title2";
+            this.chart2.Titles.Add(title3);
+            this.chart2.Titles.Add(title4);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // chart3
+            // 
+            chartArea3.BorderColor = System.Drawing.Color.BlanchedAlmond;
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(12, 308);
+            this.chart3.Name = "chart3";
+            series7.BorderWidth = 3;
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.Legend = "Legend1";
+            series7.Name = "Низкое";
+            series8.BorderWidth = 3;
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series8.Legend = "Legend1";
+            series8.Name = "Среднее";
+            series9.BorderWidth = 3;
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series9.Legend = "Legend1";
+            series9.Name = "Высокое";
+            this.chart3.Series.Add(series7);
+            this.chart3.Series.Add(series8);
+            this.chart3.Series.Add(series9);
+            this.chart3.Size = new System.Drawing.Size(425, 233);
+            this.chart3.TabIndex = 11;
+            this.chart3.Text = "chart3";
+            title5.Name = "Title1";
+            title5.Text = "Модификация методом произведения";
+            this.chart3.Titles.Add(title5);
+            // 
+            // chart4
+            // 
+            chartArea4.BorderColor = System.Drawing.Color.BlanchedAlmond;
+            chartArea4.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart4.Legends.Add(legend4);
+            this.chart4.Location = new System.Drawing.Point(12, 547);
+            this.chart4.Name = "chart4";
+            series10.BorderWidth = 3;
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series10.Legend = "Legend1";
+            series10.Name = "Объединение";
+            this.chart4.Series.Add(series10);
+            this.chart4.Size = new System.Drawing.Size(425, 233);
+            this.chart4.TabIndex = 12;
+            this.chart4.Text = "chart4";
+            title6.Name = "Title1";
+            title6.Text = "Результат объединения нечетких множеств";
+            this.chart4.Titles.Add(title6);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(444, 547);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 794);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Lab10WinForms/Lab10WinForms/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab10WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            chart1.Series["Низкое"].Points.Clear();
+            for (int i = 1; i <= 12; i++)
+            {
+                chart1.Series["Низкое"].Points.AddXY(i, MN_Low(i));
+            }
+            chart1.Series["Среднее"].Points.Clear();
+            for (int i = 1; i <= 12; i++)
+            {
+                chart1.Series["Среднее"].Points.AddXY(i, MN_Medium(i));
+            }
+            chart1.Series["Высокое"].Points.Clear();
+            for (int i = 1; i <= 12; i++)
+            {
+                chart1.Series["Высокое"].Points.AddXY(i, MN_High(i));
+            }
+
+
+
+
+            chart2.Series["Низкое"].Points.Clear();
+            for (int i = 1; i <= 16; i++)
+            {
+                chart2.Series["Низкое"].Points.AddXY(i, MP_Low(i));
+            }
+            chart2.Series["Среднее"].Points.Clear();
+            for (int i = 1; i <= 16; i++)
+            {
+                chart2.Series["Среднее"].Points.AddXY(i, MP_Medium(i));
+            }
+            chart2.Series["Высокое"].Points.Clear();
+            for (int i = 1; i <= 16; i++)
+            {
+                chart2.Series["Высокое"].Points.AddXY(i, MP_High(i));
+            }
+
+
+        }
+
+        //Нечеткое подмножество "низкое", определенное на множестве значений количества выдач
+        static float MN_Low(float n)
+        {
+            if (n <= 3) return 1;
+            if (n > 3 && n < 6) return (6 - n) / 3;
+            if (n >= 6) return 0;
+            return 0;
+        }
+        //Нечеткое подмножество "среднее", определенное на множестве значений количества выдач
+        static float MN_Medium(float n)
+        {
+            if (n < 3 || n > 9) return 0;
+            if (n >= 3 && n < 6) return (n - 3) / 3;
+            if (n >= 6 && n <= 9) return (9 - n) / 3;
+            return 0;
+        }
+        //Нечеткое подмножество "высокое", определенное на множестве значений количества выдач
+        static float MN_High(float n)
+        {
+            if (n < 6) return 0;
+            if (n >= 6 && n < 9) return (n - 6) / 3;
+            if (n >= 9) return 1;
+            return 0;
+        }
+
+        static float MP_Low(float n)
+        {
+            if (n <= 4) return 1;
+            if (n > 4 && n < 8) return (8 - n) / 4;
+            if (n >= 8) return 0;
+            return 0;
+        }
+
+        static float MP_Medium(float n)
+        {
+            if (n < 4 || n > 12) return 0;
+            if (n >= 4 && n < 8) return (n - 4) / 4;
+            if (n >= 8 && n <= 12) return (12 - n) / 4;
+            return 0;
+        }
+
+        static float MP_High(float n)
+        {
+            if (n < 8) return 0;
+            if (n >= 8 && n < 12) return (n - 8) / 4;
+            if (n >= 12) return 1;
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float MN_Low_r = MN_Low(Convert.ToSingle(textBox1.Text));
+            float MN_Medium_r = MN_Medium(Convert.ToSingle(textBox1.Text));
+            float MN_High_r = MN_High(Convert.ToSingle(textBox1.Text));
+
+            label2.Text = $"MN_Low = {MN_Low_r}";
+            label3.Text = $"MN_Medium = {MN_Medium_r}";
+            label4.Text = $"MN_High = {MN_High_r}";
+
+            chart3.Series["Низкое"].Points.Clear();
+            for (int i = 1; i <= 16; i++)
+            {
+                chart3.Series["Низкое"].Points.AddXY(i, MN_Low_r * MP_Low(i));
+            }
+            chart3.Series["Среднее"].Points.Clear();
+            for (int i = 1; i <= 16; i++)
+            {
+                chart3.Series["Среднее"].Points.AddXY(i, MN_Medium_r * MP_Medium(i));
+            }
+            chart3.Series["Высокое"].Points.Clear();
+            for (int i = 1; i <= 16; i++)
+            {
+                chart3.Series["Высокое"].Points.AddXY(i, MN_High_r * MP_High(i));
+            }
+
+            float[] resPlot = new float[17];
+
+            chart4.Series["Объединение"].Points.Clear();
+            for (int i = 1; i <= 16; i++)
+            {
+                resPlot[i] = Math.Max(MN_High_r * MP_High(i), Math.Max(MN_Medium_r * MP_Medium(i), MN_Low_r * MP_Low(i)));
+                chart4.Series["Объединение"].Points.AddXY(i, resPlot[i]);
+                
+            }
+
+            float sumi = 0;
+            float sumq = 0;
+            for (int i = 1; i <= 16; i++)
+            {
+                sumi += i * resPlot[i];
+                sumq += resPlot[i];
+            }
+            label5.Text = $"Центр тяжести полученной фигуры находится в точке {sumi / sumq}";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
